26a40cab1cd13a834055028ff0f40c60b5f5b038
gabi.rod.braga@hotmail.com <gabi.rod.braga@hotmail.com>
2025-11-10 21:26:13 +0000

    ajustes

--- silver/stops.py
+++ silver/stops.py
@@ -1,12 +1,38 @@
 # Databricks notebook source
-df = (spark.read
-                .option("header", "true")      # força sem header
-                .option("inferSchema", "true")
-                .option("delimiter", ",")
-                .csv("/Volumes/sp_trans/raw/carga_fria/stops.txt"))
-# display(df)
-
+df = spark.table("sp_trans.raw.stops")
 
 # COMMAND ----------
 
-df.write.mode("overwrite").saveAsTable("sp_trans.raw.stops")
+import unicodedata
+import re
+from pyspark.sql.functions import udf, col
+from pyspark.sql.types import StringType
+
+# Função para limpar texto
+def limpar_texto(texto):
+    if texto is None:
+        return None
+    # Normaliza (remove acentos)
+    texto = unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode('utf-8')
+    # Remove caracteres não ASCII
+    texto = re.sub(r'[^\x00-\x7F]+', '', texto)
+    # Remove espaços extras
+    texto = re.sub(r'\s+', ' ', texto)
+    return texto.strip()
+
+# Registrar UDF
+limpar_texto_udf = udf(limpar_texto, StringType())
+
+# COMMAND ----------
+
+for c, tipo in df.dtypes:
+    if tipo == "string":
+        df = df.withColumn(c, limpar_texto_udf(col(c)))
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+df.write.mode("overwrite").saveAsTable("sp_trans.trusted.stops")
